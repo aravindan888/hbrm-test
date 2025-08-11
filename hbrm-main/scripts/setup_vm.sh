9d50f0d35e2623df247db2ece5fca17656448437
Aravindan M <aravim80@gmail.com>
2025-08-11 13:35:00 +0530

    Update setup_vm.sh

--- hbrm-main/scripts/setup_vm.sh
+++ hbrm-main/scripts/setup_vm.sh
@@ -2,7 +2,7 @@
 
 # Clone repo if not present
 if [ ! -d "n8n-hbrm" ]; then
-  git clone https://github.com/YOUR_USERNAME/n8n-hbrm.git
+  git clone https://github.com/aravindan888/n8n-hbrm.git
 fi
 
 cd n8n-hbrm
